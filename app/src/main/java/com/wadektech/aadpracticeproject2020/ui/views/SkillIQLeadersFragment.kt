d81452c8b8b68d031b7ffc2f137d58eb5b3ebcac
Derrick Wadek <wadeq@Derricks-MacBook-Air.local>
2020-09-05 13:07:54 +0300

    initial commit

---
+++ app/src/main/java/com/wadektech/aadpracticeproject2020/ui/views/SkillIQLeadersFragment.kt
@@ -0,0 +1,25 @@
+package com.wadektech.aadpracticeproject2020.ui.views
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.wadektech.aadpracticeproject2020.R
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [SkillIQLeadersFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class SkillIQLeadersFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_skill_i_q_leaders, container, false)
+    }
+
+}
