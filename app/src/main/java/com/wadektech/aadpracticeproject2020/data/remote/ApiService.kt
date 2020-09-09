6b3720b334f479c7a319a8655efa7fc302756c30
Derrick Wadek <wadeq@Derricks-MacBook-Air.local>
2020-09-09 12:52:37 +0300

    Added post functionality to codebase

--- app/src/main/java/com/wadektech/aadpracticeproject2020/data/remote/ApiService.kt
+++ app/src/main/java/com/wadektech/aadpracticeproject2020/data/remote/ApiService.kt
@@ -1,11 +1,14 @@
 package com.wadektech.aadpracticeproject2020.data.remote
 
 import com.wadektech.aadpracticeproject2020.data.domainModels.LearningLeaders
+import com.wadektech.aadpracticeproject2020.data.domainModels.Response
 import com.wadektech.aadpracticeproject2020.data.domainModels.SkillIqLeaders
-import com.wadektech.aadpracticeproject2020.data.domainModels.Submit
 import kotlinx.coroutines.Deferred
-import okhttp3.Call
-import retrofit2.http.*
+import retrofit2.Call
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface ApiService {
 
@@ -18,7 +21,10 @@ interface ApiService {
     @POST("1FAIpQLSf9d1TcNU6zc6KR8bSEM41Z1g1zl35cwZr2xyjIhaMAz8WChQ/formResponse")
     @FormUrlEncoded
     fun postProjectToGoogleForm(
-        @Body submit: Submit
-    )
+        @Field("entry.1824927963") email : String,
+        @Field("entry.1877115667") firstName : String,
+        @Field("entry.2006916086") lastName : String,
+        @Field("entry.284483984") link : String
+    ) : Call<Response>
 
 }
